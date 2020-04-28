ce8383ba21898b5a6faec87bf7edfa63388da82f
Or1992 <orrahamim92@gmail.com>
2020-04-28 15:23:50 +0300

    extra file missing again :)

---
+++ includes/dateError.php
@@ -0,0 +1,41 @@
+<?php
+//include auth_session.php file on all user panel pages
+require('db.php');
+include("auth_session.php");
+?>
+
+<?php
+
+include 'header.php';
+
+?>
+
+
+ <!-- ======= HTML Section ======= -->
+
+ <main id="main">
+
+<section class="about contor-bg" >
+<div class="container RformBox">
+     <div class="row">
+      <div class="col-lg-6 contor-bg">
+
+
+<h3>התאריך שבחרת עבר, אנא בחר תארין תקין</h3>
+<button type="button"><a href="openGame.php"> חזרה </a></button>
+
+     </div>
+ </div>
+</div>
+</section>
+
+</main>
+
+<!-- ======= Footer  ======= -->
+
+
+<?php
+
+include 'footer.php';
+
+?>
---
+++ includes/error.php
@@ -0,0 +1,40 @@
+<?php
+//include auth_session.php file on all user panel pages
+require('db.php');
+include("auth_session.php");
+?>
+
+<?php
+
+include 'header.php';
+
+?>
+
+
+ <!-- ======= HTML Section ======= -->
+
+ <main id="main">
+
+<section class="about contor-bg" >
+<div class="container RformBox">
+     <div class="row">
+      <div class="col-lg-6 contor-bg">
+
+
+<h1>אירעה שגיאה</h1>
+
+     </div>
+ </div>
+</div>
+</section>
+
+</main>
+
+<!-- ======= Footer  ======= -->
+
+
+<?php
+
+include 'footer.php';
+
+?>
